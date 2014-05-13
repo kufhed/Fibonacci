e6d80b9a4c33f40a1617148db3fd63b64915264a
Kukuh Frehadtomo <kufhed@gmail.com>
2014-05-13 11:18:37 +0700

    Sum of fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,7 +11,7 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-
+      int hasil=0, []rec;
    public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
        System.out.print("Enter the value of n: ");
@@ -20,6 +20,7 @@ public class Fibonacci {
    }
 
    public static void fibonacci(int n) {
+      rec=new int[n];
        if (n == 0) {
            System.out.println("0");
        } else if (n == 1) {
@@ -30,10 +31,16 @@ public class Fibonacci {
            int b = 1;
            for (int i = 1; i < n; i++) {
                int nextNumber = a + b;
-               System.out.print(nextNumber + " ");
+               rec[i]=nextNumber
                a = b;
                b = nextNumber;
            }
+           for(int j=0; j<n;j++)
+           {
+              hasil=hasil+rec[i];
+           }
+           System.out.println(hasil);
        }
    }
 }
+//ASUMSI KODINGAN SUDAH BENAR
